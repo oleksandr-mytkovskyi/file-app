0b36621c3355acd4f0611262b3b05301bdc4b177
Alex Mytkovskyi <askona1995@gmail.com>
2021-04-01 17:32:23 +0300

    added fb auth and merge with available user

--- app/controllers/auth.controller.js
+++ app/controllers/auth.controller.js
@@ -2,6 +2,11 @@ const auth = require('../utils/googleAuth');
 const authServices = require('../services/auth.services');
 const { default: axios } = require('axios');
 const googleAuth = require('../utils/googleAuth');
+const facebookAuth = require('../utils/facebookAuth');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
 
 // google auth
 exports.redirectGoogle = (req, res, next) => {
@@ -14,7 +19,6 @@ exports.loginGoogle = async (req, res, next) => {
 
     const {tokens} = await auth.Oauth2Client.getToken(code);
     auth.Oauth2Client.setCredentials(tokens);
-    console.log(tokens);
     const {access_token, refresh_token, scope, token_type, id_token} = tokens;
     //отримали дані про користувача з google арі
     const dataAccessToken = await axios.get(`https://www.googleapis.com/oauth2/v1/tokeninfo?access_token=${access_token}`);
@@ -28,19 +32,30 @@ exports.loginGoogle = async (req, res, next) => {
             gUserId: dataAccessToken.data.user_id,
         }
     }
-    authServices.loginSocial(req, res, next, userData);
+    authServices.loginSocial(req, res, next, userData, {type: 'google'});
 }
 // Facebook auth
 exports.redirectFacebook = (req, res, next) => {
-    // const url = googleAuth.getConnectionUrl();
-    // res.redirect(url);
-    res.send('fb redirect');
+    const url = facebookAuth.facebookLoginUrl;
+    res.redirect(url);
 }
 // Facebook auth
-exports.loginFacebook = (req, res, next) => {
-    // const url = googleAuth.getConnectionUrl();
-    // res.redirect(url);
-    res.send('fb Login');
+exports.loginFacebook = async (req, res, next) => {
+
+    const fbCode = req.query.code;
+    const fbApiUrl = `https://graph.facebook.com/oauth/access_token?client_id=${process.env.FACEBOOK_CLIENT_ID}&redirect_uri=${process.env.FACEBOOK_REDIRECT_URI}&client_secret=${process.env.FACEBOOK_APP_SECRET}&code=${fbCode}`;
+    const dataAccessToken = await axios.get(fbApiUrl);
+    const fbApiUrl2 = `https://graph.facebook.com/me?fields=email,name&access_token=${dataAccessToken.data.access_token}`;
+    const userData = await axios.get(fbApiUrl2);
+    const {name, email, id} = userData.data;
+    const newUserData = {
+        name:name,
+        email: email,
+        socialAuth: {
+            fbUserId: id
+        }
+    }
+    authServices.loginSocial(req, res, next, newUserData, {type: 'facebook'});
 }
 
 
